ade34db2178d6f34093b7403f84b89010275083d
Calvin Cao <calvincao18@gmail.com>
2018-03-24 18:33:39 -0400

    URL list obtained

--- Basic_JSON_Scrapper.py
+++ Basic_JSON_Scrapper.py
@@ -1,15 +1,15 @@
-import urllib.request, json
-import re
-import time
-import requests
-
 '''
 with urllib.request.urlopen("https://www.rottentomatoes.com/api/private/v2.0/browse?maxTomato=100&maxPopcorn=100&services=amazon%3Bhbo_go%3Bitunes%3Bnetflix_iw%3Bvudu%3Bamazon_prime%3Bfandango_now&certified&sortBy=release&type=dvd-streaming-all") as url:
     data = json.loads(url.read().decode())
     print(data)
 '''
 
+# Below is the basic scrapper which can obtain and save all pages of the DVD-Streaming-All section from Rottentomatoes.com as one text file.
 def run(url):
+    import urllib.request, json
+    import re
+    import time
+    import requests
     pageNum=467
     fw=open('C:/Users/calvi/Dropbox/Desktop/RT_DVD_Streaming_All_JSON.txt','w')
     for i in range(1,pageNum+1):
@@ -33,4 +33,25 @@ if __name__=='__main__':
     url='https://www.rottentomatoes.com/api/private/v2.0/browse?maxTomato=100&maxPopcorn=100&services=amazon%3Bhbo_go%3Bitunes%3Bnetflix_iw%3Bvudu%3Bamazon_prime%3Bfandango_now&certified&sortBy=release&type=dvd-streaming-all'
     run(url)
 
-# This basic scrapper can obtain and save all pages of the DVD-Streaming-All section from Rottentomatoes.com as one text file.
+
+# Find following urls in data_str
+# Attempt 2:
+def run1():
+    import re
+    with open('C:/Users/calvi/Dropbox/Desktop/RT_DVD_Streaming_All_JSON.txt', 'r') as myfile:
+        data=myfile.read().replace('\n', '')
+    # print(data)
+    data_str = str(data)
+    url_location = [m.start() for m in re.finditer("'url':", data_str)]
+    # print(url_location)
+    fw = open('C:/Users/calvi/Dropbox/Desktop/RT_DVD_Streaming_All_URLs.txt','w')
+    for x in url_location:
+        url_start = x
+        url_end = data_str.find(',',url_start)
+        url = data_str[url_start+8:url_end-1]
+        movie_url = 'www.rottentomatoes.com'+url
+        fw.write(str(movie_url)+'\n')
+    fw.close()
+
+if __name__=='__main__':
+    run1()
