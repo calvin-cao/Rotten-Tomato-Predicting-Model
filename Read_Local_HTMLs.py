b9eadb65636b1d139ea81ddefee6932b1981a92c
Calvin Cao <calvincao18@gmail.com>
2018-04-16 17:58:16 -0400

    windows update

--- Read_Local_HTMLs.py
+++ Read_Local_HTMLs.py
@@ -1,2 +1,6 @@
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(open("C:/Local/RT/Random100/3-backyards.html"), "html.parser")
+# Do your things below (Example):
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
