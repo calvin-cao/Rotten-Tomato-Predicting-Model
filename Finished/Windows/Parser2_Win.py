9357921bdc237598d17ca2cf45ea4f635e21738b
Smit Mehta <mehta.smit92@gmail.com>
2018-08-13 16:45:44 -0400

    updated AIA.append to replace '|' by '-' in reviews

--- Finished/Windows/Parser2_Win.py
+++ Finished/Windows/Parser2_Win.py
@@ -1,96 +1,108 @@
+
 from bs4 import BeautifulSoup
 import re
 import os
 import time
 from lxml import etree
 from time import gmtime, strftime
-PATH = input("Please enter html files' path: \n")
+PATH = input("Please enter html files' path:")
 if len(PATH) < 1:
-    PATH = "C:/Local/RT/RT_All_Gen1_12_Movie_Page_Sources_HTML/"
+    PATH = "C:/local/RT_All_Gen1_12_Movie_Page_Sources_HTML/"
 start_time = time.clock()
-def run(List):
-	asdaf = 0
+
+
+def getMovieInfo(List):
 	for x in List:
 		soup = BeautifulSoup(open(x),'lxml')
-		asdaf += 1
-		print(str(asdaf))
-		try:
+		l.write(x.replace(PATH, '').replace('.html', '') + '-')
+		
+		try:    #Tomatometer
 			CS = soup.find('span',{'class':'meter-value superPageFontColor'}).text
 		except:
 			CS = None
-			print('CS Failed')
-			#continue
-		try:
+			l.write('Tomatometer Failed, ')
+			
+		try:	#Audience Score
 			AS = soup.find('span',{'style':'vertical-align:top'}).text
 		except:
 			AS = None
-			print('AS Failed')
-			#continue
-		try:
+			l.write('Audience Score Failed, ')
+			
+		try:	#Critics Consensus
 			CC = soup.find('p',{'class':'critic_consensus superPageFontColor'}).text.split('\n')[2].strip()
 		except:
 			CC = None
-			print('CC Failed')
-			#continue
-		# a = int(soup.find('span',{'class':'subtle superPageFontColor'}).text[:-1])
-		try:
+			l.write('Critic Consensus Failed, ')
+			
+		try:	#Movie Summary
 			MI = soup.find('div',{'id':'movieSynopsis'}).text.split('\n')[1].strip()
 		except:
 			MI = None
-			print('MI Failed')
-			#continue
-		try:
-			ARS = {}
+			l.write('Movie Summary Failed, ')
+			
+		try:	#Movie Details
+			MD = {}
 			for i in range(len(soup.findAll('div',{'class':re.compile('meta-label subtle')}))):
-				ARS[str(soup.findAll('div',{'class':re.compile('meta-label subtle')})[i].text.strip()[:-1].replace(' ','_').replace('/','_'))] = soup.findAll('div',{'class':re.compile('meta-value')})[i].text.strip()
-			try:
-				RA = str(ARS['Rating'])
+				MD[str(soup.findAll('div',{'class':re.compile('meta-label subtle')})[i].text.strip()[:-1].replace(' ','_').replace('/','_'))] = soup.findAll('div',{'class':re.compile('meta-value')})[i].text.strip()
+				
+			try:	#Rating
+				RA = str(MD['Rating'])
 			except:
 				RA = None
-				print('RA failed')
-			try:
-				GE = re.sub('\s+', ' ', str(ARS['Genre']).replace('\n', ''))
+				l.write('Rating Failed, ')
+				
+			try:	#Genre
+				GE = re.sub('\s+', ' ', str(MD['Genre']).replace('\n', ''))
 			except:
 				GE = None
-				print('GE failed')
-			try:
-				DB = str(ARS['Directed_By'])
+				l.write('Genre Failed, ')
+				
+			try:	#Director
+				DB = str(MD['Directed_By'])
 			except:
 				DB = None
-				print('DB failed')
-			try:
-				WB = str(ARS['Written_By'])
+				l.write('Director Failed, ')
+				
+			try:	#Writer
+				WB = str(MD['Written_By'])
 			except:
 				WB = None
-				print('WB failed')
-			try:
-				ST = str(ARS['Studio'])
+				l.write('Writer Failed, ')
+				
+			try:	#Studio
+				ST = str(MD['Studio'])
 			except:
 				ST = None
-				print('ST failed')
-			try:
-				ITD = str(ARS['In_Theaters']) #[:str(ARS['In_Theaters']).find('\n')] + '¬†'
+				l.write('Studio Failed, ')
+				
+			try:	#In Theatres Release Date
+				ITD = str(MD['In_Theaters'])
 			except:
 				ITD = None
-				print('ITD failed')
-			try:
-				ODSD = str(ARS['On_Disc_Streaming'])
+				l.write('In Theatres Date Failed, ')
+				
+			try:	#On Disc Streaming Date
+				ODSD = str(MD['On_Disc_Streaming'])
 			except:
 				ODSD = None
-				print('ODSD failed')
-			try:
-				BO = str(ARS['Box_Office'])
+				l.write('ODSD Failed, ')
+				
+			try:	#Box Office
+				BO = str(MD['Box_Office'])
 			except:
 				BO = None
-				print('BO failed')
-			try:
-				RU = str(ARS['Runtime'])
+				l.write('Box Office Failed, ')
+				
+			try:	#Runtime
+				RU = str(MD['Runtime'])
 			except:
 				RU = None
-				print('RU failed')
+				l.write('Runtime Failed, ')
+				
 		except:
-			print('CT failed')
-		try:
+			l.write('Movie Details Failed, ')
+			
+		try:	#Cast
 			castSection = soup.find('div',{'class':'castSection'})
 			Cast = ''
 			for c in castSection.findAll('a',{'href':re.compile('/celebrity/')}):
@@ -100,28 +112,28 @@ def run(List):
 			CA = Cast[:-1]
 		except:
 			CA = None
-			print('CA Failed')
-			#continue
-		try:
+			l.write('Cast Failed, ')
+			
+		try:	#Critics Review
 			CriticSection = soup.find('div',{'id':'reviews'})
 			Critic_Reviews = ''
 			for i in range(0, len(CriticSection.findAll('div',{'class':re.compile('media-body')})), 2):
-				Critic_Reviews += (CriticSection.findAll('div',{'class':re.compile('media-body')})[i].text.split('\n\n')[1].strip() + ' ')
+				Critic_Reviews += (CriticSection.findAll('div',{'class':re.compile('media-body')})[i].text.split('\n\n')[1].strip() + '-x-')
 			CR = Critic_Reviews
 		except:
 			CR = None
-			print('CR Failed')
-			#continue
-		try:
+			l.write('Critics Reviews Failed, ')
+			
+		try:	#Audience Reviews
 			Audience_Reviews = ''
 			for c in soup.findAll('p',{'class':re.compile('comment clamp clamp-6')}):
-				Audience_Reviews += (c.text.strip() + ' ')
+				Audience_Reviews += (c.text.strip() + '-x-')
 			AR = Audience_Reviews
 		except:
 			AR = None
-			print('AR Failed')
-			#continue
-		try:
+			l.write('Audience Reviews Failed, ')
+			
+		try:	#Critics Reviews Counted
 			g = open(x)
 			html = g.read().encode('utf-8')
 			selector = etree.HTML(html)
@@ -131,34 +143,69 @@ def run(List):
 			RC = reviews_counted[0].text
 		except:
 			RC = None
-			print('RC Failed')
-		try:
+			l.write('Reviews Counted Failed, ')
+			
+		try:	#User Rating
 			audiences=selector.xpath('//*[@id="scorePanel"]/div[2]/div[2]')
 			for audience in audiences:
 				users_rating_text=audience.xpath('//*[@id="scorePanel"]/div[2]/div[2]/div[2]/text()')
-				users_rating=users_rating_text[1].split()[0]
+			users_rating=users_rating_text[1].split()[0]
 			UR = users_rating
 		except:
 			UR = None
-			print('UR Failed')
-		AIA.append(str(List[asdaf - 1])[str(List[asdaf - 1]).find('HTML/') + 5:str(List[asdaf - 1]).find('.html')] + '\t' + str(AS).replace('\n', '').replace('\t', '') + '\t' + str(CS).replace('\n', '').replace('\t', '') + '\t' + str(CA).replace('\n', " ").replace('\t', ' ') + '\t' + 'None' + '\t' + str(MI).replace('\n', " ").replace('\t', ' ') + '\t' + str(ITD).replace('\n', '').replace('\t', '') + '\t' + str(GE).replace('\n', "").replace('\t', '') + '\t' + str(ST).replace('\n', " ").replace('\t', ' ') + '\t' + str(DB).replace('\n', " ").replace('\t', ' ') + '\t' + str(RU).replace('\n', " ").replace('\t', ' ') + '\t' + str(BO).replace('\n', " ").replace('\t', ' ') + '\t' + str(RA).replace('\n', " ").replace('\t', ' ') + '\t' + str(WB).replace('\n', " ").replace('\t', ' ') + '\n')
+			l.write('User Rating Failed, ')
+		  
+		try:	#Critics Avg Rating
+			car = selector.xpath('//*[@id="scoreStats"]/div[1]')
+			for cars in car:
+				cavgrating = cars.xpath('//*[@id="scoreStats"]/div[1]/text()')
+			cavgr = cavgrating[1].split()[0]
+		except:
+			cavgr = None
+			l.write('Critics Avg Rating Failed, ')
+
+		try:	#User Avg Rating
+			uar = selector.xpath('//*[@id="scorePanel"]/div[2]/div[2]/div[1]')
+			for uars in uar:
+				uavgrating = uars.xpath('//*[@id="scorePanel"]/div[2]/div[2]/div[1]/text()')
+			uavgr = uavgrating[1].split()[0]
+		except:
+			uavgr = None
+			l.write('User Avg Rating Failed, ')
+
+		try:	#Movie Name
+			if soup.find('span', {'class':'glyphicon glyphicon-play'}):
+				MovieName = soup.find('h1',{'data-type':'title'}).text.strip()
+			else:
+				MovieName = soup.find('h1',{'data-type':'title'}).text.strip()[:-4]
+		except:
+			l.write('Movie Name Failed, ')
+			
+			
+		AIA.append(x.replace(PATH, '').replace('.html', '') + '|' + str(MovieName).replace('|', '-') + '|' + str(AS).replace('\n', '').replace('\t', '') + '|' + str(CS).replace('\n', '').replace('\t', '') + '|' + str(CA).replace('\n', " ").replace('\t', ' ') + '|' + str(MI).replace('\n', " ").replace('\t', ' ').replace('|', '-') + '|' + str(ITD).replace('\n\xa0', ' ').replace('\t', '') + '|' + str(ODSD).replace('\t', '') + '|' + str(GE).replace('\n', "").replace('\t', '').replace('|', '-') + '|' + str(ST).replace('\n', " ").replace('\t', ' ') + '|' + str(DB).replace('\n', " ").replace('\t', ' ') + '|' + str(RU).replace('\n', '').replace('\t', ' ') + '|' + str(BO).replace('\n', '').replace('\t', '') + '|' + str(RA).replace('\n', '').replace('\t', ' ').replace('|', '-') + '|' + str(WB).replace('\n', '').replace('\t', ' ') + '|' + str(cavgr).replace('\n', '').replace('\t', '') + '|' + str(uavgr).replace('\n', '').replace('\t', '') + '|' + str(RC).replace('\n', '').replace('\t', '') + '|' + str(UR).replace('\n', '').replace('\t', '') + '|' + str(CR).replace('\n', '').replace('\t', '').replace('|', '-') + '|' + str(AR).replace('\n', '').replace('\t', '').replace('|', '-') + '|' + str(CC).replace('\n', ' ').replace('\t', ' ').replace('|', '-') + '\n')
+		l.write(' Done \n')
+
+		
+		
 AIA = []
+l = open(PATH + 'log_getMovieInfo.txt', 'a')
 
-run([PATH + x for x in os.listdir(PATH) if '.html' in x])
-"""
-fh = open("C:/Users/calvi/Dropbox/Desktop/sample.txt",'r',encoding = 'utf-8')
-a = []
-for l in fh:
-    n = l.split('\t')[0] + '.html'
-    if n in os.listdir('C:/Local/RT/RT_All_Gen1_12_Movie_Page_Sources_HTML/'):
-        a.append('C:/Local/RT/RT_All_Gen1_12_Movie_Page_Sources_HTML/' + n)
-fh.close()
 
-run(a)
-"""
-fh = open(PATH + 'test_' + strftime("%Y-%m-%d_%H-%M-%S") + '.txt', 'w', encoding = 'utf-8')
-fh.write('movie_id' + '\t' + 'audience_score' + '\t' + 'critic_score' + '\t' + 'actor_names' + '\t' + 'actor_links' + '\t' + 'synopsis' + '\t' + 'In Theaters' + '\t' + 'Genre' + '\t' + 'Studio' + '\t' + 'Directed By' + '\t' + 'Runtime' + '\t' + 'Box Office' + '\t' + 'Rating' + '\t' + 'Written By' + '\n')
+getMovieInfo([PATH + x for x in os.listdir(PATH) if '.html' in x])
+
+l.close()
+
+
+
+f = open(PATH + 'RT_' + strftime("%Y-%m-%d_%H-%M-%S") + '.txt', 'w', encoding = 'utf-8')
+f.write('movie_id' + '|' + 'movie_name' + '|' + 'Audience_Score' + '|' + 'Tomatometer' + '|' + 'Cast' + '|' + 'Movie_Summary' + '|' + 'In_Theaters_Release_Date' + '|' + 'On_Disc_Streaming_Date' + '|' + 'Genre' + '|' + 'Studio' + '|' + 'Directed By' + '|' + 'Runtime' + '|' + 'Box Office' + '|' + 'Rating' + '|' + 'Written By' + '|' + 'Critics_AvgRating' + '|' + 'Audience_AvgRating' + '|' + 'Critics_ReviewsCounted' + '|' + 'Audience_ReviewsCounted' + '|' + 'Critics_Reviews' + '|' + 'Audience_Reviews' + '|' + 'Critics_Consensus' + '\n')
+
 for x in AIA:
-	fh.write(x)
-fh.close()
+	f.write(x)
+
+f.close()
+
 print("--- %s seconds ---" % (time.clock() - start_time))
+
+
+
